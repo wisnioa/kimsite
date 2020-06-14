6ea8ce9759c9188c0243442b3a7de9bd9952508e
unknown <amanda.wisniowski@gmail.com>
2020-06-14 10:32:26 -0500

    layout change, file additions, etc etc

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,21 @@
+
+
+
 function myFunction(x) {
-    x.classList.toggle("change");
-  }
+x.classList.toggle("change");
+}
+
+$(document).ready(function(){
+  $('.sidenav').sidenav({
+ // Default is 300
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: false, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true // Choose whether you can drag to open on touch screens
+    }
+  );
+  // START OPEN
+  // $('.button-collapse').sideNav('show');
+});
+
+
+
